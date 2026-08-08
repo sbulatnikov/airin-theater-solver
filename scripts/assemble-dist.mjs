8ec0a9e76c598443d2c07a728ff020f023aa8b99
Sergey Bulatnikov <sergey.bulatnikov@icloud.com>
2026-08-09 00:55:10 +0200

    fix(ui): optimize mobile layouts

--- scripts/assemble-dist.mjs
+++ scripts/assemble-dist.mjs
@@ -96,7 +96,7 @@ await writeFile(
 <html lang="ru">
   <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
     <meta name="referrer" content="no-referrer">
     <meta http-equiv="Content-Security-Policy" content="default-src 'none'; base-uri 'none'; form-action 'none'">
     <meta http-equiv="refresh" content="0; url=./v2/">
